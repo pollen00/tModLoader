55e557189fc81548e04e58bb54d7491ce8ee8ba4
Stevie <xxlennygamerxx@gmail.com>
2020-10-17 21:43:21 -0700

    Finished important IDs

--- patches/Avalon/Terraria/ID/BuffID.Avalon.cs
+++ patches/Avalon/Terraria/ID/BuffID.Avalon.cs
@@ -2,6 +2,44 @@ namespace Terraria.ID
 {
 	partial class BuffID
 	{
-		public const int Count = 327;
-    }
+		public const int CrimsonDrain = 327;
+		public const int Shockwave = 328;
+		public const int Delirium = 329;
+		public const int Freeze = 330;
+		public const int Luck = 331;
+		public const int BloodCast = 332;
+		public const int Starbright = 333;
+		public const int Vision = 334;
+		public const int DarkInferno = 335;
+		public const int FracturingArmor = 336;
+		public const int Strong = 337;
+		public const int GPS = 338;
+		public const int PrimeArms = 339;
+		public const int TimeShift = 340;
+		public const int IceGolem = 341;
+		public const int Hungry = 342;
+		public const int ElectrifiedTwo = 343;
+		public const int Gastropod = 344;
+		public const int Blah = 345;
+		public const int Shadows = 346;
+		public const int Rogue = 347;
+		public const int Gauntlet = 348;
+		public const int Wisdom = 349;
+		public const int Invincibility = 350;
+		public const int InvincibilityCooldown = 351;
+		public const int Titanskin = 352;
+		public const int InfernoTwo = 353;
+		public const int BeeSweet = 354;
+		public const int ForceField = 355;
+		public const int Infected = 356;
+		public const int Miniweapon = 357;
+		public const int WeaponImbuePathogen = 358;
+		public const int BrokenWeaponry = 359;
+		public const int Unloaded = 360;
+		public const int BlackHoleSuck = 361;
+		public const int ElectrifiedThree = 362;
+		public const int ReflectorMinion = 363;
+		public const int ShadowCurse = 364;
+		public const int Count = 365;
+	}
 }
--- patches/Avalon/Terraria/ID/WallID.Avalon.cs
+++ patches/Avalon/Terraria/ID/WallID.Avalon.cs
@@ -2,6 +2,57 @@ namespace Terraria.ID
 {
 	partial class WallID
 	{
-        public const ushort Count = 316;
-    }
+		public const ushort OrangeBrickUnsafe = 316;
+		public const ushort OrangeSlabUnsafe = 317;
+		public const ushort OrangeTiledUnsafe = 318;
+		public const ushort OrangeBrickWall = 319;
+		public const ushort OrangeSlabWall = 320;
+		public const ushort OrangeTiledWall = 321;
+		public const ushort HeartstoneBrick = 322;
+		public const ushort ObsidianLavaTube = 323;
+		public const ushort ChlorophyteBrickUnused = 324;
+		public const ushort DarkMatterSlimeBlock = 325;
+		public const ushort BlueLihzahrdBrick = 326;
+		public const ushort TPBlueLihzahrdBrick = 327;
+		public const ushort Vertebrae = 328;
+		public const ushort ChocolateCandyCaneBlock = 329;
+		public const ushort RhodiumBrickWall = 330;
+		public const ushort OsmiumBrickWall = 331;
+		public const ushort ChunkstoneWall = 332;
+		public const ushort ContagionGrassWall = 333;
+		public const ushort LeadBrickWall = 334;
+		public const ushort IronBrickWall = 335;
+		public const ushort FeroziumBrickWall = 336;
+		public const ushort PyroscoricBrickWall = 337;
+		public const ushort DurataniumBrickWall = 338;
+		public const ushort NaquadahBrickWall = 339;
+		public const ushort TroxiniumBrickWall = 340;
+		public const ushort AmethystGemWall = 341;
+		public const ushort TopazGemWall = 342;
+		public const ushort SapphireGemWall = 343;
+		public const ushort EmeraldGemWall = 344;
+		public const ushort RubyGemWall = 345;
+		public const ushort DiamondGemWall = 346;
+		public const ushort ImperviousBrickWall = 347;
+		public const ushort RedVelvetCandyCaneWall = 348;
+		public const ushort MintChocolateCandyCaneWall = 349;
+		public const ushort RainbowCandyCaneWall = 350;
+		public const ushort DarkMatterWoodWall = 351;
+		public const ushort PlasticBrickWall = 352;
+		public const ushort PurpleBrickWallUnsafe = 353;
+		public const ushort PurpleSlabWallUnsafe = 354;
+		public const ushort PurpleTiledWallUnsafe = 355;
+		public const ushort PurpleBrickWall = 356;
+		public const ushort PurpleSlabWall = 357;
+		public const ushort PurpleTiledWall = 358;
+		public const ushort EctoplasmWall = 359;
+		public const ushort OblivionBrickWall = 360;
+		public const ushort ImperviousBrickUnsafe = 361;
+		public const ushort TropicalMudWall = 362;
+		public const ushort TropicsGrassWall = 363;
+		public const ushort TuhrtlBrickWallUnsafe = 364;
+		public const ushort TuhrtlBrickWallSafe = 365;
+		public const ushort NestBlockWall = 366;
+		public const ushort Count = 367;
+	}
 }
