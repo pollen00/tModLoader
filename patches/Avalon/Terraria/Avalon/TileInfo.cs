a13dc246fe60b97dca91cc261160b01411d3d4a9
Stevie <xxlennygamerxx@gmail.com>
2020-10-17 21:48:28 -0700

    Add TileInfo struct

---
+++ patches/Avalon/Terraria/Avalon/TileInfo.cs
@@ -0,0 +1,23 @@
+﻿namespace Terraria.Avalon
+{
+	public struct TileInfo
+	{
+		public int tileID;
+		public int tileStyle;
+		public int tileSlope;
+		public int wallID;
+		public int liquidType;
+		public int liquidAmount;
+		public int wire;
+
+		public TileInfo(int tileID, int tileStyle, int tileSlope, int wallID, int liquidType, int liquidAmount, int wire) {
+			this.tileID = tileID;
+			this.tileStyle = tileStyle;
+			this.tileSlope = tileSlope;
+			this.wallID = wallID;
+			this.liquidType = liquidType;
+			this.liquidAmount = liquidAmount;
+			this.wire = wire;
+		}
+	}
+}
